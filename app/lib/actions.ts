076d8d4119c925ab386e0b715651cf8cdd1d4c01
haulian <165844791+haulian@users.noreply.github.com>
2025-05-15 22:36:55 -0400

    chapter15

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -25,19 +25,55 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0]
 
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `
 
+    } catch (error) {
+        console.error(`Creating data error: ${error}`)
+    }
     revalidatePath('/dashboard/invoices')
     redirect('/dashboard/invoices')
+
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status')
+    })
+    const amountInCents = amount * 100;
+
+    try {
+        await sql`
+                UPDATE invoices 
+                SET customer_id=${customerId}, amount=${amountInCents}, status= ${status}
+                WHERE id=${id}
+            `
+
+    } catch (error) {
+        console.error(`Updating Invoice Error: ${error}`)
+    }
+    revalidatePath('/dashboard/invoices')
+    redirect('/dashboard/invoices')
+
 }
 
 
 export async function deleteInvoice(id: string) {
-    await sql`
-        DELETE FROM invoices WHERE id=${id}
-    `
+
+    throw new Error('Failed to delete invoice')
+
+    try {
+        await sql`
+            DELETE FROM invoices WHERE id=${id}
+        `
+
+    } catch (error) {
+        console.error(`Deleting data error: ${error}`)
+    }
     revalidatePath('/dashboard/invoices')
 }
